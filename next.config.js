725c7ab2944e649f166cd2e434582727f46d8e99
Juan Miguel Torres <juanmiguel.torres@engagis.com>
2020-04-14 08:58:16 +0800

    add .env file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 const withSass = require('@zeit/next-sass');
-module.exports = withSass({
-  /* config options here */
+const nextRuntimeDotenv = require('next-runtime-dotenv');
+const withPlugins = require('next-compose-plugins');
+
+const withConfig = nextRuntimeDotenv({
+  path: '.env',
+  public: ['API_URL'],
 });
+
+module.exports = withConfig(withPlugins([withSass]));
--- src/helpers/axios.tsx
+++ src/helpers/axios.tsx
@@ -1,6 +1,12 @@
+import getConfig from "next/config";
 import axios from "axios";
 
+const {
+  publicRuntimeConfig: { API_URL }, // Available both client and server side
+} = getConfig();
+
 let axiosIntance = { ...axios };
-axiosIntance.defaults.baseURL = "http://localhost:3000/";
+
+axiosIntance.defaults.baseURL = API_URL || "http://localhost:3000";
 
 export default axiosIntance;
